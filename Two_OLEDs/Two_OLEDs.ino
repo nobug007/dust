5636cbf64c024130854cf5771bbe7780f0b09778
nobug007 <nobug007@gmail.com>
2019-10-04 16:17:28 +0900

    Create Two_OLEDs.ino

---
+++ Two_OLEDs/Two_OLEDs.ino
@@ -0,0 +1,99 @@
+#include <ESP8266WiFi.h>
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
+ 
+ 
+// Initialize the OLED display using Wire library
+SSD1306Wire  display(0x3c, D2, D3);  //D2=SDK  D1=SCK  As per labeling on NodeMCU
+ 
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+ 
+void setup() {
+  delay(1000);
+  Serial.begin(115200);  
+  Serial.println("");
+  
+  Serial.println("Initializing OLED Display");
+  display.init();
+ 
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+}
+ 
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+ // drawFontFaceDemo();
+//  delay(1000);
+  drawRectDemo();
+//  delay(1000);
+//  void drawCircleDemo();
+  delay(1000);
+}
+//=========================================================================
+ 
+ 
+void drawFontFaceDemo() {
+  // clear the display
+  display.clear();
+    // Font Demo1
+    // create more fonts at http://oleddisplay.squix.ch/
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(0, 0, "Hello world");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(0, 10, "Hello world");
+    display.setFont(ArialMT_Plain_24);
+    display.drawString(0, 26, "Hello world");
+  // write the buffer to the display
+  display.display();
+}
+ 
+void drawRectDemo() {
+  // clear the display
+  display.clear();
+      // Draw a pixel at given position
+    for (int i = 0; i < 64; i++) {
+      display.setPixel(i, i);
+      display.setPixel(64 - i, i);
+    }
+display.drawCircle(32, 32, 10);
+    /*
+    display.drawRect(12, 12, 20, 20);
+ 
+    // Fill the rectangle
+    display.fillRect(14, 14, 17, 17);
+ 
+    // Draw a line horizontally
+    display.drawHorizontalLine(0, 40, 20);
+ 
+    // Draw a line horizontally
+    display.drawVerticalLine(40, 0, 20);
+   // write the buffer to the display
+   */
+  display.display();
+}
+ 
+void drawCircleDemo() {
+  // clear the display
+  display.clear();
+
+    display.fillRect(14, 14, 17, 17);
+//  display.drawRect(12, 12, 20, 20);
+//  display.setColor(WHITE);
+//  display.drawCircle(32, 32, 10);
+//    display.setColor(WHITE);
+//  for (int i=1; i < 8; i++) {
+//    display.drawCircle(32, 32, i*3);
+//    if (i % 2 == 0) {
+//      display.setColor(BLACK);
+//    }
+//    display.fillCircle(96, 32, 32 - i* 3);
+ // }
+ 
+  // write the buffer to the display
+  display.display();
+}
