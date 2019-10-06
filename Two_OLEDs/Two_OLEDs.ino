c34dc2aa6ea676ef57d31e40ce345c8738247990
Bang Hyo sik <nobug007@gmail.com>
2019-10-06 22:25:24 +0900

    Update Two_OLEDs.ino

--- Two_OLEDs/Two_OLEDs.ino
+++ Two_OLEDs/Two_OLEDs.ino
@@ -9,6 +9,8 @@ SSD1306Wire  display(0x3c, D2, D3);  //D2=SDK  D1=SCK  As per labeling on NodeMC
 //=======================================================================
 //                    Power on setup
 //=======================================================================
+
+ int i=0;
  
 void setup() {
   delay(1000);
@@ -20,13 +22,16 @@ void setup() {
  
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
+  display.clear();
+  drawFontFaceDemo();
 }
  
 //=======================================================================
 //                    Main Program Loop
 //=======================================================================
 void loop() {
- // drawFontFaceDemo();
+//  if ( i== 17 ) i =0;
+   drawFontFaceDemo();
 //  delay(1000);
   drawRectDemo();
 //  delay(1000);
@@ -43,24 +48,48 @@ void drawFontFaceDemo() {
     // create more fonts at http://oleddisplay.squix.ch/
     display.setTextAlignment(TEXT_ALIGN_LEFT);
     display.setFont(ArialMT_Plain_10);
-    display.drawString(0, 0, "Hello world");
-    display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 10, "Hello world");
-    display.setFont(ArialMT_Plain_24);
-    display.drawString(0, 26, "Hello world");
-  // write the buffer to the display
+
+  display.drawRect(0, 0, display.getStringWidth("BT ON")+6, 16);
+    display.drawString(3, 2, "BT ON");
+
+    display.setTextAlignment(TEXT_ALIGN_RIGHT);
+    display.setFont(ArialMT_Plain_10);
+  display.drawRect(122-display.getStringWidth("WiFi ON"),0,display.getStringWidth("WiFi ON")+6, 16);
+    display.drawString(125,2, "WiFi ON");
+
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(64,2, "DUST");
+
+
+     display.drawRect(0, 17, 128, 47);
+     display.drawLine(0, 40, 128, 40);     //  47/2 = 23
+     display.drawLine(100, 18, 100, 63);     //  47/2 = 23
+
   display.display();
 }
  
 void drawRectDemo() {
   // clear the display
-  display.clear();
+//  display.clear();
+   display.setTextAlignment(TEXT_ALIGN_RIGHT);
+   display.setFont(ArialMT_Plain_10);
+   display.drawString(124,25, "888");
+   display.drawString(124,48, "000");
+
+  for(i=0;i<100;i++) {
+      display.drawLine(i,40,i,35+sin(i)*3 );
+  }
+   for(i=0;i<100;i++) {
+      display.drawLine(i,67,i,62+cos(i)*3 );
+  }
+//  display.drawHorizontalLine(0,i++,128);
       // Draw a pixel at given position
-    for (int i = 0; i < 64; i++) {
-      display.setPixel(i, i);
-      display.setPixel(64 - i, i);
-    }
-display.drawCircle(32, 32, 10);
+//    for (int i = 0; i < 64; i++) {
+//      display.setPixel(i, i);
+//      display.setPixel(64 - i, i);
+//    }
+//display.drawCircle(32, 32, 10);
     /*
     display.drawRect(12, 12, 20, 20);
  
