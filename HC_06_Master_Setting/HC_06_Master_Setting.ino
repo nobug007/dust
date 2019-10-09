16959eae873461f3af2c5e8ccacb944dc4daa7a5
Bang Hyo sik <nobug007@gmail.com>
2019-10-09 23:41:47 +0900

    1
    1

---
+++ HC_06_Master_Setting/HC_06_Master_Setting.ino
@@ -0,0 +1,48 @@
+#include <SoftwareSerial.h>
+SoftwareSerial bluetooth(2, 3);
+void setup() 
+{
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  
+  Serial.println("BT Write !");
+
+  bluetooth.print("AT");
+  delay(1000);
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+
+  
+  bluetooth.print("AT+ROLE=M");
+  delay(1000);
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+
+  bluetooth.print("AT+NAMEDust_M");
+  delay(1000);
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+  bluetooth.print("AT+PIN1234");
+  delay(1000);
+
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+
+  Serial.println("OK All Done !");
+
+}
+void loop()
+{
+}
---
+++ HC_06_Slave_Setting/HC_06_Slave_Setting.ino
@@ -0,0 +1,48 @@
+#include <SoftwareSerial.h>
+SoftwareSerial bluetooth(6, 7);
+void setup() 
+{
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  
+  Serial.println("BT Write !");
+
+  bluetooth.print("AT");
+  delay(1000);
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+
+  
+  bluetooth.print("AT+ROLE=S");
+  delay(1000);
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+
+  bluetooth.print("AT+NAMEDust_S");
+  delay(1000);
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+  bluetooth.print("AT+PIN1234");
+  delay(1000);
+
+  while(bluetooth.available()) {
+    char c = bluetooth.read();
+    Serial.print(c);
+  }
+  Serial.println(" !");
+
+  Serial.println("OK All Done !");
+
+}
+void loop()
+{
+}
