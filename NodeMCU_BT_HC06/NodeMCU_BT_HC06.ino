643caa9a3d8481501316af2e76d009dd975cf887
nobug <nobug007@gmail.com>
2020-01-05 15:53:46 +0900

    BT Update

--- NodeMCU_BT_HC06/NodeMCU_BT_HC06.ino
+++ NodeMCU_BT_HC06/NodeMCU_BT_HC06.ino
@@ -1,7 +1,7 @@
 //
 //  FILE : NodeMCU_BT_HC06
 //  AUTHOR : nobug (nobug007@gmail.com)
-//  CREATED : 4.1.2020
+//  CREATED : 5.1.2020
 //  HW : NodeMCU & HC06
 //
 #include <string.h>
@@ -9,7 +9,7 @@
 #include <SoftwareSerial.h>
 
 // BT Port Set up
-SoftwareSerial bluetooth(D1, D2);
+SoftwareSerial bluetooth(D1, D2);   //  TX =  D1, RX =  D2    VCC = UV ( test ) 5V
 
 
 //=======================================================================
@@ -21,6 +21,7 @@ void setup() {
   delay(1000);
   Serial.begin(9600);
   // BT setup
+  Serial.println("BT Start");
   bluetooth.begin(9600);
 }
 
@@ -28,20 +29,28 @@ void setup() {
 //                    Main Program Loop
 //=======================================================================
 void loop() {
+  BT_Read();
+}
+
+void BT_Read() {
   char out_data[10];
   int out_i=0;
   char Flag = 'N';
 
   while (bluetooth.available()) {
+      Serial.print(".");
     // 수신 받은 데이터 저장
     out_data[out_i] = (char)bluetooth.read();
     out_data[++out_i] = NULL;
-    // 수신된 데이터 시리얼 모니터로 출력
     Flag = 'Y';
+    delay(100);
   }
+  
   if ( Flag == 'Y' ) {
+       // 수신된 데이터 시리얼 모니터로 출력
         Serial.print("BT Read Out Data :  ");
         Serial.println(out_data);
+        Flag = 'N';
   }
 }
 //=========================================================================
